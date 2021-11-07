af23524c1c271f41f34bfee9b7090ca3514b1cd3
NSISLV <info@nsis.lv>
2021-11-07 16:27:16 +0200

    Send email

--- flaskapp.py
+++ flaskapp.py
@@ -1,4 +1,7 @@
 import flask
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 # A simple Flask App which takes
 # a user's name as input and responds
@@ -8,10 +11,20 @@ app = flask.Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    message = ''
     if flask.request.method == 'POST':
-        message = 'Hello ' + flask.request.form['name-input'] + '!'
-    return flask.render_template('index.html', message=message)
+    
+        # Create message container - the correct MIME type is multipart/alternative here!
+        sender = flask.request.form['from']
+        recipients = flask.request.form['to']
+        msg = flask.request.form['message']
+    
+        # Create SMTP object
+        server = smtplib.SMTP(host='localhost')
+        server.sendmail(sender, recipients, msg.as_string())
+        server.quit()
+        return flask.render_template('index.html')
+      
+    return flask.render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
